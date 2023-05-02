27f37e9678eeedb3b76220bda3a3c311b0ed681d
AnGeLZhiL <angelina.zhilyakova.2002@mail.ru>
2023-05-02 22:34:47 +0300

    Добавление моделей, реквестов и контроллеров

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,41 +4,32 @@ namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
-
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
+    protected $primaryKey = 'id_user';
     protected $fillable = [
-        'name',
-        'email',
-        'password',
+        'last_name',
+        'first_name',
+        'midlle_name',
+        'login',
+        'password'
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
     protected $hidden = [
-        'password',
-        'remember_token',
+        'password'
     ];
 
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
+    public function tests() : BelongsToMany {
+        return $this->belongsToMany(
+            Tests::class,
+            'user_test',
+            'user_id',
+            'test_id'
+        );
+    }
 }
